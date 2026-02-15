0097d00ab2cd7228ec54387262fdfec375dc34b4
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-02-15 13:00:22 -0500

    implemented human chocie func

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ function getComputerChoice() {
   if (randomNum <= 0.33) {
     compChoice = "rock";
   } else if (randomNum > 0.33 && randomNum <= 0.66) {
-    compChoice = "paper";
   } else {
+    compChoice = "paper";
     compChoice = "scissors";
   }
+  return compChoice;
 }
+
+function getHumanChoice() {
+  let userChoice = prompt("Pick your choice: Rock, Paper, or Scissors");
+  return userChoice;
+}
+
+console.log(getHumanChoice());
